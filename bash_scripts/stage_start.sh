7062770d5394ecf8414d0b1fa22fce00c04e2dc0
Egor Alexandrov <egor4ik@gmail.com>
2019-12-10 11:03:31 +0300

    scripts are looking at the FisGo_F repo

--- bash_scripts/stage_start.sh
+++ bash_scripts/stage_start.sh
@@ -3,7 +3,7 @@
 TOKEN=$1
 CONTEXT="$2"
 
-curl "https://api.GitHub.com/repos/egorsego/docker-build/statuses/$GIT_COMMIT?access_token=$TOKEN" \
+curl "https://api.GitHub.com/repos/dreamkas/FisGo_F/statuses/$GIT_COMMIT?access_token=$TOKEN" \
 -H "Content-Type: application/json" \
 -X POST \
--d "{\"state\": \"pending\", \"context\": \"Jenkins-CI: ${CONTEXT} Stage\", \"target_url\": \"http://192.168.242.43:8080/job/pr_test/$BUILD_NUMBER\"}"
+-d "{\"state\": \"pending\", \"context\": \"Jenkins-CI: ${CONTEXT} Stage\", \"target_url\": \"http://192.168.242.43:8080/job/dreamkas-f-ci-pipeline-test/$BUILD_NUMBER\"}"
