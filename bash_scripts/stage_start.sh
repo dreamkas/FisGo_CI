179e4f277c076213c3b5edf2ce3ac4da5f33f45e
Egor Alexandrov <egor4ik@gmail.com>
2019-12-11 15:37:51 +0300

    context naming to status checks on github

--- bash_scripts/stage_start.sh
+++ bash_scripts/stage_start.sh
@@ -3,13 +3,10 @@
 TOKEN=$1
 CONTEXT="$2"
 
-echo "Token: $TOKEN"
-echo "Context: $CONTEXT"
 echo "Commit: $GIT_COMMIT"
 echo "Branch: $GIT_BRANCH"
-
 
 curl "https://api.GitHub.com/repos/dreamkas/FisGo_F/statuses/$GIT_COMMIT?access_token=$TOKEN" \
 -H "Content-Type: application/json" \
 -X POST \
--d "{\"state\": \"pending\", \"context\": \"Jenkins-CI: ${CONTEXT} Stage\", \"target_url\": \"http://192.168.242.43:8080/job/dreamkas-f-ci-pipeline-test/$BUILD_NUMBER\"}"
+-d "{\"state\": \"pending\", \"context\": \"Jenkins-CI: [${CONTEXT} Stage]\", \"target_url\": \"http://192.168.242.43:8080/job/dreamkas-f-ci-pipeline-test/$BUILD_NUMBER\"}"
