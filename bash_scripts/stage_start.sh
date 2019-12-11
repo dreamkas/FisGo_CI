7b6a46ab16b51023bf4e32e13456d380d3b93975
Egor Alexandrov <egor4ik@gmail.com>
2019-12-11 12:05:48 +0300

    another debug message added

--- bash_scripts/stage_start.sh
+++ bash_scripts/stage_start.sh
@@ -6,6 +6,8 @@ CONTEXT="$2"
 echo "Token: $TOKEN"
 echo "Context: $CONTEXT"
 echo "Commit: $GIT_COMMIT"
+echo "Branch: $GIT_BRANCH"
+
 
 curl "https://api.GitHub.com/repos/dreamkas/FisGo_F/statuses/$GIT_COMMIT?access_token=$TOKEN" \
 -H "Content-Type: application/json" \
