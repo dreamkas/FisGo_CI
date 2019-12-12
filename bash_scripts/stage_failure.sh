2effbbde22639665d08f7aa50cbeb8ce2c5529d8
Egor Alexandrov <egor4ik@gmail.com>
2019-12-12 11:32:31 +0300

    args with spaces

--- bash_scripts/stage_failure.sh
+++ bash_scripts/stage_failure.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-TOKEN=$1
-CONTEXT="$2"
+args=("$@")
+TOKEN=${args[0]}
+CONTEXT=${args[1]}
+
+echo "Commit: $GIT_COMMIT"
+echo "Branch: $GIT_BRANCH"
+echo "Context: $CONTEXT"
 
 curl "https://api.GitHub.com/repos/dreamkas/FisGo_F/statuses/$GIT_COMMIT?access_token=$TOKEN" \
 -H "Content-Type: application/json" \
 -X POST \
--d "{\"state\": \"failure\", \"context\": \"Jenkins-CI: [${CONTEXT} Stage]\", \"description\": \"Failed Miserably\", \"target_url\": \"http://192.168.242.43:8080/job/dreamkas-f-ci-pipeline-test/$BUILD_NUMBER\"}"
+-d "{\"state\": \"failure\", \"context\": \"Jenkins-CI: [$CONTEXT Stage]\", \"description\": \"Failed Miserably\", \"target_url\": \"http://192.168.242.43:8080/job/dreamkas-f-ci-pipeline-test/$BUILD_NUMBER\"}"
