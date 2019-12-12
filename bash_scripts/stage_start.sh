3266419c57a2f2ea5cc59db8a4897e36dbb1692c
Egor Alexandrov <egor4ik@gmail.com>
2019-12-12 12:54:34 +0300

    removed hardcoded build url

--- bash_scripts/stage_start.sh
+++ bash_scripts/stage_start.sh
@@ -11,4 +11,4 @@ echo "Context: $CONTEXT"
 curl "https://api.GitHub.com/repos/dreamkas/FisGo_F/statuses/$GIT_COMMIT?access_token=$TOKEN" \
 -H "Content-Type: application/json" \
 -X POST \
--d "{\"state\": \"pending\", \"context\": \"Jenkins-CI: [$CONTEXT Stage]\", \"target_url\": \"http://192.168.242.43:8080/job/dreamkas-f-ci-pipeline-test/$BUILD_NUMBER\"}"
+-d "{\"state\": \"pending\", \"context\": \"Jenkins-CI: [$CONTEXT Stage]\", \"target_url\": \"$BUILD_URL\"}"
