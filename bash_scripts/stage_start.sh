86162289b566cea181097d8ef8f6ccdae09d9466
Egor Alexandrov <egor4ik@gmail.com>
2019-12-12 13:16:10 +0300

    link to results in blue ocean

--- bash_scripts/stage_start.sh
+++ bash_scripts/stage_start.sh
@@ -11,4 +11,4 @@ echo "Context: $CONTEXT"
 curl "https://api.GitHub.com/repos/dreamkas/FisGo_F/statuses/$GIT_COMMIT?access_token=$TOKEN" \
 -H "Content-Type: application/json" \
 -X POST \
--d "{\"state\": \"pending\", \"context\": \"Jenkins-CI: [$CONTEXT Stage]\", \"target_url\": \"$BUILD_URL\"}"
+-d "{\"state\": \"pending\", \"context\": \"Jenkins-CI: [$CONTEXT Stage]\", \"target_url\": \"$RUN_DISPLAY_URL\"}"
