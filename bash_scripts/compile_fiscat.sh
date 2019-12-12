d349e6956f5d4a6ae2773e209423717a7284674e
Egor Alexandrov <egor4ik@gmail.com>
2019-12-12 18:51:11 +0300

    run script inside container

--- bash_scripts/compile_fiscat.sh
+++ bash_scripts/compile_fiscat.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-CONTAINER=$1
-
-docker run --name $CONTAINER dreamkas-sf-library /bin/bash
 mkdir build
 cd /build/
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=On ..
--- bash_scripts/compile_units.sh
+++ bash_scripts/compile_units.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-CONTAINER=$1
-
-docker run --name $CONTAINER -it dreamkas-sf-library /bin/bash
 sed 's/#add_definitions(-DG_UNIT)/add_definitions(-DG_UNIT)/g' -i CMakeLists.txt
 mkdir build
 cd /build/
