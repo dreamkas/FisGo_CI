fbda57331453b67c6062e842ac9751ee9d62901c
Egor Alexandrov <egor4ik@gmail.com>
2019-12-16 15:04:36 +0300

    fewer params passing

--- bash_scripts/upload_patch.sh
+++ bash_scripts/upload_patch.sh
@@ -3,11 +3,10 @@
 args=("$@")
 TOKEN=${args[0]}
 VERSION=${args[1]}
-FROM=${args[2]}
-FILE=${args[3]}
+FILE=${args[2]}
 
 curl --request POST \
-	--url "https://update-beta.dreamkas.ru/v2/projects/fisgo/products/ea_test_product/patches/${VERSION}?version_from=${FROM}&barrier=true&enabled=false" \
+	--url "https://update-beta.dreamkas.ru/v2/projects/fisgo/products/ea_test_product/patches/${VERSION}?barrier=false&enabled=false" \
 	--header "BACKEND-TOKEN: ${TOKEN}" \
   	--header "content-type: multipart/form-data;" \
  	--form "file=@${FILE}"
