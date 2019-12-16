1259f85b7ab937fcf7513fd42eb71123ca4d4485
Egor Alexandrov <egor4ik@gmail.com>
2019-12-16 15:54:18 +0300

    print tag_name

--- bash_scripts/stage_start.sh
+++ bash_scripts/stage_start.sh
@@ -7,6 +7,7 @@ CONTEXT=${args[1]}
 echo "Commit: $GIT_COMMIT"
 echo "Branch: $GIT_BRANCH"
 echo "Context: $CONTEXT"
+echo "Tag Name: $TAG_NAME"
 
 curl "https://api.GitHub.com/repos/dreamkas/FisGo_F/statuses/$GIT_COMMIT?access_token=$TOKEN" \
 -H "Content-Type: application/json" \
