55801af41463e7a3713200e3580e604c7c7bddf5
Egor Alexandrov <egor4ik@gmail.com>
2019-12-16 14:48:46 +0300

    arguments added

--- bash_scripts/upload_patch.sh
+++ bash_scripts/upload_patch.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-VERSION=${args[0]}
-FROM=${args[1]}
-FILE=${args[2]}
+args=("$@")
+TOKEN=${args[0]}
+VERSION=${args[1]}
+FROM=${args[2]}
+FILE=${args[3]}
 
 curl --request POST \
 	--url "https://update-beta.dreamkas.ru/v2/projects/fisgo/products/ea_test_product/patches/${VERSION}?version_from=${FROM}&barrier=true&enabled=false" \
-	--header "BACKEND-TOKEN: 6ebbbff4-6017-4f47-b41c-daf2aee0c06d" \
+	--header "BACKEND-TOKEN: ${TOKEN}" \
   	--header "content-type: multipart/form-data;" \
  	--form "file=@${FILE}"
