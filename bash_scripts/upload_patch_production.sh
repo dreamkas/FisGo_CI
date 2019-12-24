8a655ae98a8d115ae4c08e6d2418e97215172c79
Egor Alexandrov <egor4ik@gmail.com>
2019-12-24 15:01:36 +0300

    md5 of a patch argument passing

--- bash_scripts/upload_patch_production.sh
+++ bash_scripts/upload_patch_production.sh
@@ -4,9 +4,10 @@ args=("$@")
 TOKEN=${args[0]}
 VERSION=${args[1]}
 FILE=${args[2]}
+MD5=${args[3]}
 
 curl --request POST \
-	--url "https://update.dreamkas.ru/v2/projects/fisgo/products/dreamkasFRedirect/patches/${VERSION}?barrier=false&enabled=false" \
+	--url "https://update.dreamkas.ru/v2/projects/fisgo/products/dreamkasFRedirect/patches/${VERSION}?md5=${MD5}&barrier=false&enabled=false" \
 	--header "BACKEND-TOKEN: ${TOKEN}" \
   	--header "content-type: multipart/form-data;" \
  	--form "file=@${FILE}"
