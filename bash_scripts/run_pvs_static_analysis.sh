6c399ada7353e1e41d4a01b8cf8ef594fa9995f4
Egor Alexandrov <egor4ik@gmail.com>
2020-01-10 10:49:37 +0300

    generating xml report

--- bash_scripts/run_pvs_static_analysis.sh
+++ bash_scripts/run_pvs_static_analysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ./build/
-mkdir report
+mkdir pvs_report
 pvs-studio-analyzer analyze -j4 --disableLicenseExpirationCheck
-plog-converter -a GA:1,2 -t fullhtml -o ./report ./PVS-Studio.log
+plog-converter -a GA:1,2 -t xml -o ./pvs_report/report.xml ./PVS-Studio.log
