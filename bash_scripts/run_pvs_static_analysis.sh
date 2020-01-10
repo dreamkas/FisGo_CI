1b57479e2817567cea36060ea7174c58ff40dc6f
Egor Alexandrov <egor4ik@gmail.com>
2020-01-10 11:07:52 +0300

    path change

--- bash_scripts/run_pvs_static_analysis.sh
+++ bash_scripts/run_pvs_static_analysis.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cd ./build/
-mkdir pvs_report
 pvs-studio-analyzer analyze -j4 --disableLicenseExpirationCheck
-plog-converter -a GA:1,2 -t xml -o ./pvs_report/report.xml ./PVS-Studio.log
+plog-converter -a GA:1,2 -t xml -o ./pvs_report.xml ./PVS-Studio.log
