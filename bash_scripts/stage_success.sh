b5397800f625a67245a753e47ba6fc8b52d978dc
kormilicinkostia <kormilicinkostia@gmail.com>
2020-01-17 10:50:40 +0300

    update docer file

--- bash_scripts/stage_success.sh
+++ bash_scripts/stage_success.sh
@@ -8,7 +8,7 @@ echo "Commit: $GIT_COMMIT"
 echo "Branch: $GIT_BRANCH"
 echo "Context: $CONTEXT"
 
-curl "https://api.GitHub.com/repos/dreamkas/Dreamkas_F/statuses/$GIT_COMMIT?access_token=$TOKEN" \
+curl "https://api.GitHub.com/repos/dreamkas/pirit2f_ppp/statuses/$GIT_COMMIT?access_token=$TOKEN" \
 -H "Content-Type: application/json" \
 -X POST \
 -d "{\"state\": \"success\", \"context\": \"Jenkins-CI: [$CONTEXT]\", \"description\": \"Completed Successfully\", \"target_url\": \"$RUN_DISPLAY_URL\"}"
