8db865714864d6bdadf46b4375121e4ca2643e4e
Egor Alexandrov <egor4ik@gmail.com>
2020-01-17 14:40:54 +0300

    compile units

--- bash_scripts/compile_units_kassarb.sh
+++ bash_scripts/compile_units_kassarb.sh
@@ -2,6 +2,6 @@
 
 mkdir build
 cd ./build/
-cmake -DBUILD_DREAMKAS_RB=ON -BUILD_UNITS=ON -DBUILD_DREAMKAS_RF=OFF ..
+cmake -DBUILD_DREAMKAS_RF=OFF -DBUILD_DREAMKAS_RB=ON -BUILD_UNITS=ON ..
 make -j4
 exit $?
--- bash_scripts/compile_units_sputnikf.sh
+++ bash_scripts/compile_units_sputnikf.sh
@@ -2,6 +2,6 @@
 
 mkdir build
 cd ./build/
-cmake -DBUILD_SPUTNIK_F=ON -DBUILD_UNITS=ON -DBUILD_DREAMKAS_RF=OFF ..
+cmake -DBUILD_DREAMKAS_RF=OFF -DBUILD_SPUTNIK_F=ON -DBUILD_UNITS=ON ..
 make -j4
 exit $?
