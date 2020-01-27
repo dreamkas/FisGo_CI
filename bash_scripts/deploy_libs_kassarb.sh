251dd55315ae8e03e7954af2fbafc8d6da1e2027
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-01-27 18:49:34 +0300

    root pass change

--- bash_scripts/deploy_libs_kassarb.sh
+++ bash_scripts/deploy_libs_kassarb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
-sshpass -p root ssh-copy-id -o StrictHostKeyChecking=no root@192.168.242.191
+sshpass -p 324012 ssh-copy-id -o StrictHostKeyChecking=no root@192.168.242.191
 cd Libraries/
 ./deployLibs.bash 2 y 192.168.242.191
