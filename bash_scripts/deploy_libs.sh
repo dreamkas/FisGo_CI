a4f2414f1926a08cd60d5d07a98cd880cc0b5534
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-01-27 15:11:50 +0300

    Update deploy_libs.sh

--- bash_scripts/deploy_libs.sh
+++ bash_scripts/deploy_libs.sh
@@ -4,6 +4,6 @@ args=("$@")
 DEVICE_IP=${args[0]}
 
 sshpass -p root ssh-copy-id -o StrictHostKeyChecking=no root@$DEVICE_IP
-cd FisGo/Libraries
+cd Libraries/
 chmod + x ./deployLibs.bash
 ./deployLibs.bash 1 y $DEVICE_IP
