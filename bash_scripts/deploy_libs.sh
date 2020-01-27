1ae991b1b8d89331b9d6eb278fba81109c372be9
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-01-27 16:54:16 +0300

    Update deploy_libs.sh

--- bash_scripts/deploy_libs.sh
+++ bash_scripts/deploy_libs.sh
@@ -6,7 +6,7 @@ sshpass -p root ssh-copy-id -o StrictHostKeyChecking=no root@192.168.243.6
 echo "Done 2"
 cd Libraries/
 echo "Done 3"
-chmod + x ./deployLibs.bash
+chmod +x ./deployLibs.bash
 echo "Done 4"
 ./deployLibs.bash 1 y 192.168.243.6
 echo "Done 5"
