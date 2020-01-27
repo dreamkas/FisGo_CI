1b5dfb3973b3e3acc13e0204c2e0abd8c53577e4
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-01-27 16:40:41 +0300

    de-bugging

--- bash_scripts/deploy_libs.sh
+++ bash_scripts/deploy_libs.sh
@@ -3,8 +3,14 @@
 args=("$@")
 DEVICE_IP=${args[0]}
 
+echo "${DEVICE_IP}"
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
+echo "Done 1"
 sshpass -p root ssh-copy-id -o StrictHostKeyChecking=no root@${DEVICE_IP}
+echo "Done 2"
 cd Libraries/
+echo "Done 3"
 chmod + x ./deployLibs.bash
+echo "Done 4"
 ./deployLibs.bash 1 y ${DEVICE_IP}
+echo "Done 5"
