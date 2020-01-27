49770af821959ce23fedbea2f90b5831805912a7
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-01-27 16:20:13 +0300

    Update deploy_libs.sh

--- bash_scripts/deploy_libs.sh
+++ bash_scripts/deploy_libs.sh
@@ -3,6 +3,7 @@
 args=("$@")
 DEVICE_IP=${args[0]}
 
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
 sshpass -p root ssh-copy-id -o StrictHostKeyChecking=no root@${DEVICE_IP}
 cd Libraries/
 chmod + x ./deployLibs.bash
