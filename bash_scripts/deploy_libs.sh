1f512792ee663233b0b901106833ac7c641fdc8e
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-01-27 16:50:14 +0300

    Update deploy_libs.sh

--- bash_scripts/deploy_libs.sh
+++ bash_scripts/deploy_libs.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-args=("$@")
-DEVICE_IP=${args[0]}
-
-echo "${DEVICE_IP}"
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
 echo "Done 1"
-sshpass -p root ssh-copy-id -o StrictHostKeyChecking=no root@${DEVICE_IP}
+sshpass -p root ssh-copy-id -o StrictHostKeyChecking=no root@192.168.243.6
 echo "Done 2"
 cd Libraries/
 echo "Done 3"
 chmod + x ./deployLibs.bash
 echo "Done 4"
-./deployLibs.bash 1 y ${DEVICE_IP}
+./deployLibs.bash 1 y 192.168.243.6
 echo "Done 5"
