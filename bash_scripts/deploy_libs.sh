5be3a145361aabbb266faff7772e1e6bc1ef37cc
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-01-27 16:16:54 +0300

    Update deploy_libs.sh

--- bash_scripts/deploy_libs.sh
+++ bash_scripts/deploy_libs.sh
@@ -3,7 +3,7 @@
 args=("$@")
 DEVICE_IP=${args[0]}
 
-sshpass -p root ssh-copy-id -o StrictHostKeyChecking=no root@$DEVICE_IP
+sshpass -p root ssh-copy-id -o StrictHostKeyChecking=no root@${DEVICE_IP}
 cd Libraries/
 chmod + x ./deployLibs.bash
-./deployLibs.bash 1 y $DEVICE_IP
+./deployLibs.bash 1 y ${DEVICE_IP}
