293079b2d42ec1b9c2f6b203166f1841a58937d3
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-01-28 18:44:29 +0300

    back to hardcode

--- bash_scripts/deploy_libs.sh
+++ bash_scripts/deploy_libs.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-args=("$@")
-DEVICE_IP=${args[0]}
-
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
-sshpass -p root ssh-copy-id -o StrictHostKeyChecking=no root@${DEVICE_IP}
+sshpass -p root ssh-copy-id -o StrictHostKeyChecking=no root@192.168.243.6
 cd Libraries/
-./deployLibs.bash 1 y ${DEVICE_IP}
+./deployLibs.bash 1 y 192.168.243.6
