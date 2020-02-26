413942dda66044e6ca0590fb80d3f2aded5e3e81
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-02-26 17:20:43 +0300

    kf script removed

--- bash_scripts/run_system_tests_kf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd tmp/FisGoTests/
-git pull
-mvn clean test -Dsurefire.suiteXmlFiles=src/test/resources/kf_test.xml -DdeviceType=KASSA_F -DdeviceIP=192.168.243.124 -DhubIP=192.168.242.43 -DhubPort=8284
