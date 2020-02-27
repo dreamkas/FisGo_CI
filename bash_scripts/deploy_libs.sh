6d060edd79dd231b6ab84f3a2da7becfeed8d01e
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-02-27 15:14:27 +0300

    some scripts removed

--- bash_scripts/deploy_libs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
-sshpass -p root ssh-copy-id -o StrictHostKeyChecking=no root@192.168.243.6
-cd Libraries/
-./deployLibs.bash 1 y 192.168.243.6
