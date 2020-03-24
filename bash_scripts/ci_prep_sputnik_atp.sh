b2acdd9b8c8f11d4387943d655364027f537dc7a
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-03-24 14:02:59 +0300

    sputnik atp script added

---
+++ bash_scripts/ci_prep_sputnik_atp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sleep 5
+killall wpa_supplicant
+killall fiscat
+killall punix
+sleep 5
+/usr/sbin/wpa_supplicant -Dnl80211 -iwlan0 -c/FisGo/wpa_supplicant.conf &
+sleep 10
+/sbin/udhcpc -i wlan0
+sleep 5
+/bin/sync
+/bin/sync
+/FisGo/punix >> /FisGo/outp &
+/FisGo/fiscat -t >> /FisGo/outf &
