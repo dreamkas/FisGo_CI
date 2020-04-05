cf885362c29bfc50bbdcdcd3a113bafc8a520c0a
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-04-05 21:44:58 +0300

    Create upload_artifact.sh

---
+++ bash_scripts/upload_artifact.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+args=("$@")
+SERVER=${args[0]}
+API_TOKEN=${args[1]}
+DIRECTORY=${args[2]}
+FILE_PATH=${args[3]}
+FILE=${args[4]}
+MD5=${args[5]}
+
+curl -sSf \
+       -H "X-JFrog-Art-Api:${API_TOKEN}" \
+       -X PUT \
+       -T ${FILE_PATH} \
+       --header "X-Checksum-MD5:${MD5}" \
+       '${SERVER}/${DIRECTORY}/${FILE}'
