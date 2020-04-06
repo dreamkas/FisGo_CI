b90ee494df219e5a3fe54dc12e0d5ab26656c550
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-04-06 22:39:12 +0300

    Create release_notification.sh

---
+++ bash_scripts/release_notification.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+args=("$@")
+RELAY_SERVER=${args[0]}
+CHAT=${args[1]}
+PRODUCT_TYPE=${args[2]}
+VERSION=${args[3]}
+DOWNLOAD_LINK=${args[4]}
+
+curl --request POST \
+"${RELAY_SERVER}?chat_id=${CHAT}&parse_mode=markdown&text=Для *${PRODUCT_TYPE}* выпущена новая версия - *${VERSION}*.%0AАрхив DirPatch доступен по ссылке:%0A${DOWNLOAD_LINK}"
