efe4b81ab0c6847eb56891b43dfd3c777ae792a8
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-04-17 23:27:07 +0300

    Update release_notification.sh

--- bash_scripts/release_notification.sh
+++ bash_scripts/release_notification.sh
@@ -5,4 +5,4 @@ RELAY_SERVER=${args[0]}
 CHAT=${args[1]}
 RELEASE_NOTES=${args[2]}
 
-curl --request POST "${RELAY_SERVER}?chat_id=${CHAT}&parse_mode=html&text=Касса ${RELEASE_NOTES}"
+curl --request POST "${RELAY_SERVER}?chat_id=${CHAT}&parse_mode=markdown&text=${RELEASE_NOTES}"
