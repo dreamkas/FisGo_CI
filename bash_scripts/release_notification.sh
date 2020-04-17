7badee17c5550379ed96614d0cb11db0f0e49568
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-04-17 21:45:08 +0300

    Update release_notification.sh

--- bash_scripts/release_notification.sh
+++ bash_scripts/release_notification.sh
@@ -6,9 +6,10 @@ CHAT=${args[1]}
 PRODUCT_TYPE=${args[2]}
 VERSION=${args[3]}
 DOWNLOAD_LINK=${args[4]}
+RELEASE_NOTES=${args[5]}
 
 #curl --request POST \
 #"${RELAY_SERVER}?chat_id=${CHAT}&parse_mode=markdown&text=Для *${PRODUCT_TYPE}* выпущена новая версия - *${VERSION}*.%0AАрхив DirPatch доступен по ссылке:%0A${DOWNLOAD_LINK}"
 
 curl --request POST \
-"${RELAY_SERVER}?chat_id=${CHAT}&parse_mode=markdown&text=Для продукта *${PRODUCT_TYPE}* выпущена новая версия - *${VERSION}*.%0A[DirPatch](${DOWNLOAD_LINK})"
+"""${RELAY_SERVER}?chat_id=${CHAT}&parse_mode=markdown&text=Для продукта *${PRODUCT_TYPE}* выпущена новая версия - *${VERSION}*.%0A"${RELEASE_NOTES}"%0A[DirPatch](${DOWNLOAD_LINK})"""
