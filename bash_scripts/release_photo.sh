6ad9b17014fe999663fd6540adf738db1f9fdbbe
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-04-18 00:21:27 +0300

    Update release_photo.sh

--- bash_scripts/release_photo.sh
+++ bash_scripts/release_photo.sh
@@ -6,6 +6,6 @@ CHAT=${args[1]}
 PHOTO=${args[2]}
 RELEASE_NOTES=${args[3]}
 
-curl -s -X POST "${RELAY_SERVER}" -F chat_id="${CHAT}" -F photo=@"${PHOTO}" -F caption=${RELEASE_NOTES} -F parse_mode=markdown
+curl -s -X POST "${RELAY_SERVER}" -F chat_id="${CHAT}" -F photo=@"${PHOTO}" -F caption="${RELEASE_NOTES}" -F parse_mode=markdown
 
 #curl -s -X POST "${RELAY_SERVER}?chat_id=${CHAT}&photo=@${PHOTO}&parse_mode=markdown&caption=${RELEASE_NOTES}"
