246f5937a24184d984264e407883c5341544c6ea
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-04-19 21:30:07 +0300

    Update upload_patch_beta.sh

--- bash_scripts/upload_patch_beta.sh
+++ bash_scripts/upload_patch_beta.sh
@@ -5,7 +5,8 @@ TOKEN=${args[0]}
 VERSION=${args[1]}
 FILE=${args[2]}
 MD5=${args[3]}
+INFO=${args[4]}
 
 curl -F "file=@${FILE}" \
 -H "BACKEND-TOKEN: ${TOKEN}" \
---url "https://update-beta.dreamkas.ru/v2/projects/fisgo/products/dreamkasFRedirect/patches/${VERSION}?md5=${MD5}&barrier=false&enabled=false"
+--url "https://update-beta.dreamkas.ru/v2/projects/fisgo/products/dreamkasFRedirect/patches/${VERSION}?md5=${MD5}&barrier=false&enabled=false&info=${INFO}"
