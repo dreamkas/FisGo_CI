29321c4e696f277a61b0ba21278f833055aeb1e5
Egor Alexandrov <egorsego@users.noreply.github.com>
2020-04-24 18:26:53 +0300

    Update release_photo.sh

--- bash_scripts/release_photo.sh
+++ bash_scripts/release_photo.sh
@@ -9,7 +9,7 @@ VERSION=${args[4]}
 #DOWNLOAD_LINK=${args[5]}
 #RELEASE_NOTES=${args[6]}
 
-curl -s -X POST "${RELAY_SERVER}" -F chat_id="${CHAT}" -F photo=@"${PHOTO}" -F caption=Для *${PRODUCT_TYPE}* выпущена новая версия - *${VERSION}* -F parse_mode=markdown
+curl -s -X POST "${RELAY_SERVER}" -F chat_id="${CHAT}" -F photo=@"${PHOTO}" -F caption="Для *${PRODUCT_TYPE}* выпущена новая версия - *${VERSION}*" -F parse_mode=markdown
 
 #curl -s -X POST "${RELAY_SERVER}" -F chat_id="${CHAT}" -F photo=@"${PHOTO}" -F caption=Для *${PRODUCT_TYPE}* выпущена новая версия - *${VERSION}* "${RELEASE_NOTES}" [DirPatch](${DOWNLOAD_LINK}) -F parse_mode=markdown
 
